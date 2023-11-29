d37ef29ff4cbd1a3aa1259b12c0d2ea40b77dcbc
gayathripeesapati <135707802+gayathripeesapati@users.noreply.github.com>
2023-11-29 14:40:47 +0530

    Add files via upload

--- load_dataset.py
+++ load_dataset.py
@@ -17,29 +17,28 @@ def get_text_embedding(text):
 
 ''' Creating embeddings ''' 
 def push_vector_database():
-    data_store_disk_file = 'stb_embedding.pkl'
+    data_store_disk_file = 'data_embeddings/stb_embedding.pkl'
     if os.path.exists(data_store_disk_file):
         df = pd.read_csv(data_store_disk_file)
+        print("Loaded embedding")
     else:
-        """
-        
-        """
-        df = pd.read_excel('stb_all_details.xlsx')
+        print("Creating embedding")
+        df = pd.read_excel('data_embeddings/stb_all_details.xlsx')
         try:del df['Unnamed: 0']
         except Exception as e:print(e)
         df['full_product']=df['productName']+'\n'+df['productDescription']
         df['full_information'] = df['full_product']+df['full_address']
-        print(df.columns)
+        # print(df.columns)
         for i in ['full_product','full_information','full_address']:
-            print(df[i])
+            # print(df[i])
             df[f'{i}_vector'] = df[i].parallel_apply(get_text_embedding)
-        df.to_csv(data_store_disk_file)
+        df.to_csv(data_store_disk_file,index=False)
 
     ''' pushing to database '''
     del df['index']
     df.reset_index(inplace = True)
     df.to_sql('stb_vector_data_store',engine,index=False,if_exists='replace')
-    conn = engine.connect()
+    # conn = engine.connect()
 
     queries=['ALTER TABLE public.stb_vector_data_store ALTER COLUMN full_product_vector TYPE public.vector USING full_product_vector::public.vector::public.vector;',
     'ALTER TABLE public.stb_vector_data_store ALTER COLUMN full_information_vector TYPE public.vector USING full_information_vector::public.vector::public.vector;',
@@ -48,37 +47,46 @@ def push_vector_database():
         conn.execute(text(i))
         conn.commit()
 
-    conn.close()
+    # conn.close()
 def push_faq_file():
-    df = pd.read_csv('final.csv')
-    updated_link = []
-    updated_text = []
-    for i in range(len(df)):
-        chunks = df['about'][i].split(' ')
-        split_n = len(chunks)//100
-        if split_n<1:
-            split_n=1
-        split_text = [' '.join(i) for i in list(np.array_split(chunks,split_n))]
-        updated_text = updated_text + split_text
-        updated_link = updated_link + len(split_text)*[df['link'][i]]
-    updated_df = pd.DataFrame()
-    updated_df['link']=updated_link
-    updated_df['about']=updated_text
-    updated_df['about_vector'] = updated_df['about'].parallel_apply(get_text_embedding)
-    #updated_df['about_vector'] = updated_df['about_vector'].str.replace('{',"'[").replace('}',"]'")
-    print(updated_df)
+    data_store_disk_file =  "data_embeddings/stb_faq_embedding.pkl"
+    if os.path.exists(data_store_disk_file):
+        updated_df = pd.read_csv(data_store_disk_file)
+        print("Loaded embedding")
+    else:
+        print("Created embedding")
+        df = pd.read_csv('data_embeddings/final.csv')
+        updated_link = []
+        updated_text = []
+        for i in range(len(df)):
+            chunks = df['about'][i].split(' ')
+            split_n = len(chunks)//100
+            if split_n<1:
+                split_n=1
+            split_text = [' '.join(i) for i in list(np.array_split(chunks,split_n))]
+            updated_text = updated_text + split_text
+            updated_link = updated_link + len(split_text)*[df['link'][i]]
+        updated_df = pd.DataFrame()
+        updated_df['link']=updated_link
+        updated_df['about']=updated_text
+        updated_df['about_vector'] = updated_df['about'].parallel_apply(get_text_embedding)
+        #updated_df['about_vector'] = updated_df['about_vector'].str.replace('{',"'[").replace('}',"]'")
+        updated_df.to_csv(data_store_disk_file,index=False)
+    # print(updated_df)
     updated_df.reset_index(inplace = True)
     updated_df.to_sql('stb_vector_faq_store',engine,index=False,if_exists='replace')
-    conn = engine.connect()
+    # conn = engine.connect()
     queries=["""update stb_vector_faq_store set about_vector  = replace(about_vector,'{','[')""",
              """update stb_vector_faq_store set about_vector  = replace(about_vector,'}',']')""",
              """ALTER TABLE public.stb_vector_faq_store ALTER COLUMN about_vector TYPE public.vector USING about_vector::public.vector::public.vector;"""]
     for i in queries:
         conn.execute(text(i))
         conn.commit()
+    # conn.close()
 
-    conn.close()
-    
+conn = engine.connect()
 push_vector_database()
-print('FInished first vector database')
+print('Finished first vector database')
 push_faq_file()
+print('Finished second vector database')
+conn.close()
