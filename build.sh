8c6ae8d9b456ee4478535bdc8d2098725acb7b84
sagar290894c <121943079+sagar290894c@users.noreply.github.com>
2023-12-26 16:52:52 +0530

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-hash=`git rev-parse HEAD`
-echo $hash
-echo $BUILD_ID
-docker build . -t admin-ui:$hash
-docker tag admin-ui:$hash arytic.azurecr.io/admin-ui:$hash
-docker tag arytic.azurecr.io/admin-ui:$hash arytic.azurecr.io/admin-ui:$BUILD_ID
-echo $D_PASSWORD | docker login arytic.azurecr.io -u $D_USERNAME --password-stdin
-docker push arytic.azurecr.io/admin-ui:$BUILD_ID
