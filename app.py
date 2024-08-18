e114dff795b46c413def290e34f89e122c799ee6
sagar290894c <121943079+sagar290894c@users.noreply.github.com>
2024-08-18 20:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -404,4 +404,4 @@ if __name__ == "__main__":
 #     asyncio.run(subscribe_to_webhook())
 
 #     # Run the FastAPI application
-#     uvicorn.run(app, host="127.0.0.1", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8501)
