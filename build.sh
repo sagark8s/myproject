315634be550ca01bb28eb485ab56b21e2b920a4f
sagark8s <157510953+sagark8s@users.noreply.github.com>
2024-08-21 13:28:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,3 @@ docker tag cglense:$hash containerregistry1z.azurecr.io/cglense:$hash
 docker tag containerregistry1z.azurecr.io/cglense:$hash containerregistry1z.azurecr.io/cglense:$BUILD_ID
 echo $D_PASSWORD | docker login containerregistry1z.azurecr.io -u $D_USERNAME --password-stdin
 docker push containerregistry1z.azurecr.io/cglense:$BUILD_ID
-#
